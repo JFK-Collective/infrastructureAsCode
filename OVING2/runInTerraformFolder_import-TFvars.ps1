f2dd75620c21de4a092b9da919f57836294b5ffd
Jørgen Fenstad Kottum <jorgen.kottum@gmail.com>
2025-10-21 03:57:08 +0200

    ye

--- OVING2/runInTerraformFolder_import-TFvars.ps1
+++ OVING2/runInTerraformFolder_import-TFvars.ps1
@@ -22,14 +22,14 @@ az keyvault secret set `
 Write-Host "✓ Hele filen '$TfvarsFilePath' er lagret som secret '$SecretName' i Key Vault '$KeyVaultName'"
 
 $vaultName = "kv-tfstate-jfk"
->> 
->> # Loop through all .tfvars files and upload them Base64-encoded
->> Get-ChildItem -Filter "*.tfvars" | ForEach-Object {
->>     $envName = $_.BaseName  # "dev", "test", or "prod"
->>     $b64 = [Convert]::ToBase64String([System.IO.File]::ReadAllBytes($_.FullName))
->>     Write-Host "Uploading $envName.tfvars to Key Vault..."
->>     az keyvault secret set --vault-name $vaultName --name "$envName-tfvars-oblig2" --value $b64 | Out-Null
->> }
->> 
->> Write-Host "✅ All .tfvars uploaded to Key Vault as Base64."
->> 
+ 
+ # Loop through all .tfvars files and upload them Base64-encoded
+ Get-ChildItem -Filter "*.tfvars" | ForEach-Object {
+     $envName = $_.BaseName  # "dev", "test", or "prod"
+     $b64 = [Convert]::ToBase64String([System.IO.File]::ReadAllBytes($_.FullName))
+     Write-Host "Uploading $envName.tfvars to Key Vault..."
+     az keyvault secret set --vault-name $vaultName --name "$envName-tfvars-oblig2" --value $b64 | Out-Null
+ }
+ 
+ Write-Host "✅ All .tfvars uploaded to Key Vault as Base64."
+ 
